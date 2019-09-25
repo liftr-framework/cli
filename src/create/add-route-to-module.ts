a0fcc0dab2b31d6aa0acc6bf30e2568d51496863
Faris Tangastani <ftangastani@gmail.com>
2019-09-25 16:38:11 +0200

    fixed add route to module

--- src/create/add-route-to-module.ts
+++ src/create/add-route-to-module.ts
@@ -2,9 +2,8 @@ import { readFile, writeFile } from 'fs-extra';
 
 export const addRouteToModule = async (newRouteName: string, path: string) => {
     const file: Buffer = await readFile(process.cwd() + '/src/routes/' + path + '.module.ts');
-    const position1 = file.indexOf(`
-export const NewModule = Module([
-`) + 38;
+    const position1 = file.indexOf(`= Module([
+`) + 10;
     const output = `
     {
         route: ${newRouteName}Route,
@@ -14,7 +13,7 @@ export const NewModule = Module([
     const importStatement = `
 import { ${newRouteName}Route } from '@routes/${newRouteName}/${newRouteName}.routes';`;
     const newFileContent = [file.slice(0, position1), output, file.slice(position1)].join('');
-    const position2 = newFileContent.indexOf("import { Module } from '@liftr/core';") + 102;
+    const position2 = newFileContent.indexOf("import { Module } from '@liftr/core';") + 37;
     const finalFile = [
         newFileContent.slice(0, position2),
         importStatement,
